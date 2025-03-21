ffba3eb087ce8dd495c17757b31e143143b38ea6
Ronny Delgado <delgado.ronny@gmail.com>
2025-03-21 15:52:20 -0600

    feat: put all together in program

---
+++ hmac-based-authentication/Program.cs
@@ -0,0 +1,13 @@
+﻿using hmac_based_authentication.Receiver;
+using hmac_based_authentication.Sender;
+
+// Let's start the service that will receive the request first
+var receiverService = ReceiverService.Main();
+
+// Wait for the receiver to start
+await Task.Delay(1000);
+
+// Now, let's send a request!
+await SenderService.SendRequestAsync();
+
+await receiverService;
